b2c3395c75d9d2f3d8c7e2540b2469fe238a54bf
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-14 15:26:12 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,51 @@
-# Write your code here.
+
+
+
+
+def line(katz_deli)
+  if katz_deli.empty? 
+    puts "The line is currently empty."
+  else
+    in_line = ["The line is currently:"]
+    katz_deli.each.with_index do |name, index|
+      in_line.push( "#{index + 1}. #{name}")
+    end 
+    puts in_line.join(" ")
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+     katz_deli << name
+    puts ("Welcome, #{name}. You are number #{katz_deli.index(name) + 1} in line.")
+ end
+
+def now_serving(katz_deli)
+  if katz_deli.empty? 
+    puts  "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#1. Make a statment that starts with the line is currenty:
+# Take each name and add a number 
+# It spots the line and then adds to the statement above
+# print out the statement
+
